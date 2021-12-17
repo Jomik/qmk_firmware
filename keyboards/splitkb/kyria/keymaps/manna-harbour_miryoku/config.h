b125aac9fbc562922ae4a32ffc2208e864792416
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2021-12-17 16:13:49 +0100

    feat: add modifiers to base layer

--- keyboards/splitkb/kyria/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/splitkb/kyria/keymaps/manna-harbour_miryoku/config.h
@@ -15,10 +15,10 @@
      N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
 ) \
 LAYOUT( \
-TO(GAME), K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, TO(BASE), \
-XXX, K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, XXX, \
-XXX, K20, K21, K22, K23, K24, XXX, XXX,      XXX, XXX, K25, K26, K27, K28, K29, XXX, \
-               XXX, XXX, K32, K33, K34,      K35, K36, K37, XXX, XXX \
+TO(GAME), K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09, TO(BASE), \
+ KC_LSFT, K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, XXX, \
+ KC_RCTL, K20, K21, K22, K23, K24, XXX, XXX,      XXX, XXX, K25, K26, K27, K28, K29, XXX, \
+                XXX, KC_LALT, K32, K33, K34,      K35, K36, K37, XXX, XXX \
 )
 #else
 #define LAYOUT_miryoku( \
